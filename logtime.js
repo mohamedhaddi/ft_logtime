b20ccafb6b1be7502c69467c5d34e0af81d50b27
Mohamed Haddi <mhaddi@student.1337.ma>
2020-12-21 12:39:47 +0100

    update plugin to write on page instead of showing alert

--- logtime.js
+++ logtime.js
@@ -3,6 +3,7 @@ let url = document.getElementById('user-locations').getAttribute('data-url')
 fetch(url)
 	.then(res => res.json())
 	.then((out) => {
+
 		hour = minute = second = 0
 		for (const [key, value] of Object.entries(out)) {
 			var date = new Date()
@@ -16,10 +17,29 @@ fetch(url)
 				second += parseInt(splitTime[2])
 			}
 		}
+
 		hour += Math.floor(minute / 60);
 		minute = minute % 60 + Math.floor(second / 60);
 		second = second % 60;
-		alert('Yer logtime fer this month be ' + hour + ':' + minute + ':' + second);
+
+		logtime = 'Yer logtime fer this month be '
+			+ ("0" + hour).slice(-2) + ':'
+			+ ("0" + minute).slice(-2) + ':'
+			+ ("0" + second).slice(-2)
+
+		section_titles = document.getElementsByClassName('profile-title')
+		logtime_title_element = Array.from(section_titles).map(el => {
+			if (el.innerHTML == '\nLogtime\n') {
+				return el
+			}
+		}).filter(el => {
+			return el
+		})[0]
+
+		logtime_title_element.insertAdjacentHTML('beforeend', '<span style = "color: #00babc">' + logtime + '</span>')
+		alert('Yer logtime fer this month be '
+			+ ("0" + hour).slice(-2) + ':'
+			+ ("0" + minute).slice(-2) + ':'
+			+ ("0" + second).slice(-2))
 	})
 	.catch(err => {throw err});
-
