8f2e412e16ad24d8991c532268196011a0bfd99e
Mohamed Haddi <mhaddi@student.1337.ma>
2020-12-22 17:42:22 +0100

    fix issue with seconds and minutes reaching 60

--- ft_logtime.js
+++ ft_logtime.js
@@ -1,49 +1,60 @@
+function sumTime(array) {
+	var times = [3600, 60, 1],
+		sum = array
+			.map(s => s.reduce((s, v, i) => s + times[i] * v, 0))
+			.reduce((a, b) => a + b, 0);
+
+	return times
+		.map(t => [Math.floor(sum / t), sum %= t][0])
+		.map(v => v.toString().padStart(2, 0))
+		.join(':');
+}
+
 let url = document.getElementById('user-locations').getAttribute('data-url')
+var date = new Date()
+var year = date.getFullYear()
+var month = date.getMonth() + 1
+date = year + '-' + month
+var days = []
 
 fetch(url)
 	.then(res => res.json())
 	.then((out) => {
 
-		hour = minute = second = 0
+		let hour = minute = second = 0
 		for (const [key, value] of Object.entries(out)) {
-			var date = new Date()
-			var year = date.getFullYear()
-			var month = date.getMonth() + 1
-			date = year + '-' + month
 			if (key.substring(0, 7) == date) {
-				var splitTime = value.split(':')
-				hour += parseInt(splitTime[0])
-				minute += parseInt(splitTime[1])
-				second += parseInt(splitTime[2])
+				let time = value.split(':')
+				time[2] = time[2].split('.')[0]
+				days.push(time)
 			}
 		}
 
-		hour += Math.floor(minute / 60);
-		minute = minute % 60 + Math.floor(second / 60);
-		second = second % 60;
+		[hour, minute, second] = sumTime(days).split(':')
 
 		const monthNames = ["January", "February", "March", "April", "May", "June",
 			"July", "August", "September", "October", "November", "December"
 		];
-		logtime = 'Your ' + monthNames[month - 1] + ' logtime is '
+		let logtime = 'Your ' + monthNames[month - 1] + ' logtime is '
 			+ '<span style="text-decoration: underline; '
 			+ 'text-decoration-style: dotted; '
 			+ 'text-decoration-thickness: 2px; '
 			+ 'text-underline-offset: .2vh">'
-			+ ("0" + hour).slice(-2) + ':'
-			+ ("0" + minute).slice(-2) + ':'
-			+ ("0" + second).slice(-2)
+			+ hour + ':' + minute + ':' + second
 			+ '</span>'
 
-		section_titles = document.getElementsByClassName('profile-title')
-		logtime_title_element = Array.from(section_titles).map(el => {
-			if (el.innerHTML == '\nLogtime\n') {
+		let section_titles = document.getElementsByClassName('profile-title')
+		let logtime_title_element = Array.from(section_titles).map(el => {
+			if (el.innerHTML.indexOf("Logtime") != -1) {
 				return el
 			}
 		}).filter(el => {
 			return el
 		})[0]
 
-		logtime_title_element.insertAdjacentHTML('beforeend', '<span style = "color: #00babc; font-size: calc(.7em + .3vmin) !important">' + logtime + '</span>')
+		logtime_title_element.insertAdjacentHTML('beforeend',
+			'<span style = "color: #00babc; font-size: calc(.7em + .3vmin) !important">'
+			+ logtime
+			+ '</span>')
 	})
 	.catch(err => {throw err});
